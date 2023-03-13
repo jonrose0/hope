4250f6ce6c315e380511659021b8e6174911d688
jon <jonrose.webdev@yahoo.com>
2023-03-13 13:37:12 -0500

    deploy3

--- src/script.js
+++ src/script.js
@@ -1,14 +1,23 @@
 const el = document.querySelector('.data')
+
+console.log('tst')
+
 async function getSmartFloorPrice(contractAddress) {
   try {
-    const config = {
-      method: 'GET',
-      url: `https://your-app.vercel.app/api/smart-floor-price`,
-      params: {
-        contractAddress: contractAddress,
-      },
-    }
-    const data = await axios(config).then((res) => res.data)
+    // const tst = fetch('https://api.adviceslip.com/advice')
+    // .then(res => res.json())
+    // .then(data => el.innerHTML += data.slip.advice)
+    // .then(data => console.log(data.slip.advice))
+    // const config = {
+    //   method: 'GET',
+    //   url: `https://api.adviceslip.com/advice`,
+    //   // url: `https://your-app.vercel.app/api/smart-floor-price`,
+    //   params: {
+    //     contractAddress: contractAddress,
+    //   },
+    // }
+    // const data = await fetch(config).then((res) => res.data)
+    const data = await fetch('https://your-app.vercel.app/api/smart-floor-price').then((res) => res.json()).then(data => el.innerHTML += data.slip.advice)
     // el.innerHTML = data
     return data
   } catch (err) {
@@ -16,4 +25,10 @@ async function getSmartFloorPrice(contractAddress) {
   }
 }
 
-el.innerHTML = getSmartFloorPrice('lol')
+// el.innerHTML = getSmartFloorPrice('lol')
+
+getSmartFloorPrice()
+
+console.log(el)
+
+el.innerText = 'world'
